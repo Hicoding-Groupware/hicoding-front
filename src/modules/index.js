53f5f21b8e44f87e9c9e28a23f8c93dfc9caf326
dhnn <dhnn412@gmail.com>
2023-12-01 15:26:04 +0900

    coursepage

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,9 @@
 import {combineReducers} from "redux";
 import lectureReducer from "./LectureModule";
+import courseReducer from "./CourseModule";
 
 const rootReducer = combineReducers({
-    lectureReducer
+    lectureReducer, courseReducer
 });
 
 export default rootReducer;
