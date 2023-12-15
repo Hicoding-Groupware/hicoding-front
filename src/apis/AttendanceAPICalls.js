1a6dd963fc8643eb686173c15d096d8152d9c236
HI-SAYA <qnfnsk45@naver.com>
2023-12-15 19:45:15 +0900

    feat/#41/일별 출석부 구현 완료

--- src/apis/AttendanceAPICalls.js
+++ src/apis/AttendanceAPICalls.js
@@ -1,5 +1,5 @@
 import {authRequest} from "./Api";
-import {getStudentsInfo, getTeacherAttendance, postSuccess} from "../modules/AttendanceModule";
+import {getStudentsInfo, getTeacherAttendance, postSuccess, putSuccess} from "../modules/AttendanceModule";
 import {toast} from "react-toastify";
 import {post} from "axios";
 
@@ -11,7 +11,7 @@ export const callMyCourseStudentListAPI = ({cosCode, atdDate}) => {
 
         let url = `/attendance/day/${cosCode}`;
 
-        if(atdDate) {
+        if (atdDate) {
             url += `?atdDate=${atdDate}`;
         }
 
@@ -26,15 +26,14 @@ export const callMyCourseStudentListAPI = ({cosCode, atdDate}) => {
 }
 
 
-
 /* 출석 등록 */
-export const callAttendanceRegistAPI = ({ registRequest }) => {
+export const callAttendanceRegistAPI = ({registRequest}) => {
 
     return async (dispatch, getState) => {
 
         const result = await authRequest.post(`/attendance/day`, registRequest,
             {
-                headers : {
+                headers: {
                     'Content-Type': 'application/json'
                 }
             }).catch(e => {
@@ -47,36 +46,37 @@ export const callAttendanceRegistAPI = ({ registRequest }) => {
         console.log("registRequest : ", registRequest);
         console.log('callAttendanceRegistAPI result : ', result);
 
-        if(result?.status === 200) {
+        if (result?.status === 200) {
             dispatch(postSuccess());
         }
     }
 }
 
 
-
 /* 출석 수정 */
-export const callAttendanceUpdateAPI = ({ updateRequest, atdDate }) => {
+export const callAttendanceUpdateAPI = ({updateRequest, atdDate}) => {
 
+    console.log("atdDate : ", atdDate); // undefined
     return async (dispatch, getState) => {
 
         const result = await authRequest.put(`/attendance/day/${atdDate}`, updateRequest,
             {
-                headers : {
-                    'Content-Type' : 'application/json'
+                headers: {
+                    'Content-Type': 'application/json'
                 }
             }).catch(e => {
-                if (e.response.status === 4001) {
-                    toast.error("해당하는 과정이 없습니다.");
-                } else if (e.response.status === 5000) {
-                    toast.error("해당 학생은 이 과정을 듣지 않습니다.");
-                } // 나중에 다시 체크 ---------------------------------
+            if (e.response.status === 4001) {
+                toast.error("해당하는 과정이 없습니다.");
+            } else if (e.response.status === 5000) {
+                toast.error("해당 학생은 이 과정을 듣지 않습니다.");
+            } // 나중에 다시 체크 ---------------------------------
         });
         console.log("updateRequest : ", updateRequest);
         console.log('callAttendanceUpdateAPI result : ', result);
 
-        if(result?.status === 200) {
+        if (result?.status === 200) {
             dispatch(getTeacherAttendance(result));
+            dispatch(putSuccess());
         }
     }
 }
